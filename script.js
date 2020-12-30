80a33994b6213f100bbe4a95b7c1c00f0bf9cb74
AbdallahEssamGaber <abdallahgaber441@gmail.com>
2020-12-31 00:43:56 +0200

    V1.2
    So Mega needed a modification in the algorithm which is the user can type any number and give him a message if it's bigger or not to do that I had to delete some functionalities that I like but it's ok .... Waiting FOR THE BIG UPDATE (V2.0) :)

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const megaAchieved = 2;   //The Basic Number (Abdallah's part)
+const AGAchieved = 15;   //The Basic Number (Abdallah's part)
 
 
 //Vars control the whole program
 var inputVal = 0;
 var total = 0;
+var numOfTimes = 0;
 
 //For what can user type and can't user type
 var numPadValue1 = 96, //cuz I don't want to change it
@@ -19,12 +20,15 @@ var numField = document.getElementById('numberOfAchievements');
 
 
 
+
+
+
 //To control before user click submit(ACHIEVED)
-if ((megaAchieved == 1)) {
+if ((AGAchieved == 1)) {
   one();
 }
 
-if ((megaAchieved % 2 == 0 || megaAchieved % 2 != 0) && megaAchieved > 1) {
+if ((AGAchieved % 2 == 0 || AGAchieved % 2 != 0) && AGAchieved > 1) {
   even();
 }
 
@@ -34,47 +38,25 @@ if ((megaAchieved % 2 == 0 || megaAchieved % 2 != 0) && megaAchieved > 1) {
 //function set all the stuff to interact with the user when the number will enter is (1)
 function one() {
 
-  numPadValue2 = 98;
-  numRowValue2 = 50;
   numField.placeholder = 'Achieve One Video';
 
 }
 //function set all the stuff to interact with the user when the number will enter is (even number)
 function even() {
-  numPadValue2 = 99;
-  numRowValue2 = 51;
-  numField.placeholder = 'Achieve One Video OR Two';
+
+  numField.placeholder = 'Achieve More than One Video';
 }
 
 
-
-
-
-
-
-/*  CONTROLS FOR (BUTTONS, FIELD...) {INTERACT THINGS} */
-//Control what's user can type + some of basic stuff (Ctrl + A), (BackSpace)
-numField.onkeydown = function(e) {
-  if (!((e.keyCode > numPadValue1 && e.keyCode < numPadValue2) ||
-      (e.keyCode > numRowValue1 && e.keyCode < numRowValue2) || (e.keyCode == 17 | e.keyCode == 65) || e.keyCode == 8)) {
-    return false;
-  }
-}
-
-//Hide The button when there is no input or more than one input(Actually, the user can't write more than one number that's in the HTML sheet)
-$('#numberOfAchievements').keyup(function() {
-  if (($.trim(this.value).length > 0 && $.trim(this.value).length < 2) && $.trim(this.value)[0] != 0) $('#pressToSubmitAchievement').show();
-  else $('#pressToSubmitAchievement').hide();
-});
 
 //To make user can write and (Enter) then it's a click
 numField.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13 && $('button').is(":visible")) {
+  if (event.keyCode === 13) {
     event.preventDefault();
     document.getElementById("pressToSubmitAchievement").click();
   }
 });
-//If the user clicks on the field the input disappears so we need the button, too
+
 
 
 
@@ -82,11 +64,31 @@ numField.addEventListener("keyup", function(event) {
 
 
 function AfterClicked() {
-
+numOfTimes++;
+ document.getElementById("message2").style.color = "#c8c8c8";
+ document.getElementById("message2").innerHTML =" ";
   /*  FIRST OF ALL, THE BASICS  */
   inputVal = numField.value;    //Store the input value to a var
   console.log(inputVal);        //If the user really wanted to see the history of his inputs
+  if (inputVal > AGAchieved || parseInt(total) + parseInt(inputVal) > AGAchieved)
+  {
 
+      document.getElementById("message2").innerHTML = "Bigger than the D. ";
+      document.getElementById("message2").style.color = "#d6511f";
+
+      var x = document.createElement("SPAN");
+      document.getElementById("message2").appendChild(x);
+      x.innerHTML = " The total: " + total;
+      if (total + 1 == AGAchieved) {   //Changing the stuff cuz we like (AGAchieved = 1;)
+          one();  //Set the Stuff
+          /*  Note for the user that there is one left  */
+          x.innerHTML = "The total: " + total + " One Left.";
+        }
+      document.getElementsByTagName("SPAN")[0].style.color = "#c8c8c8";
+      return;
+
+
+  }
 
   total = parseInt(total) + parseInt(inputVal);   //Get the total to can make a decision about the input
   document.getElementById("message2").innerHTML = total + " Done.";   //The number of your achievements
@@ -95,11 +97,11 @@ function AfterClicked() {
 
 
 
-  //If He has done (megaAchieved) then CELEBRATE
-  if (total == megaAchieved) {
+  //If He has done (AGAchieved) then CELEBRATE
+  if (total == AGAchieved) {
 
-    var congrats;   //Just to change the color depending on how many (megaAchieved)
-    if (megaAchieved > 3) {
+    var congrats;   //Just to change the color depending on how many (AGAchieved)
+    if (AGAchieved > 3) {
       congrats = "<h1 id = 'congrats' style='color:#FFDD00;'>CONGRATS</h1>";
     } else {
       congrats = "<h1 id = 'congrats' style='color:#01BEFE;'>CONGRATS</h1>";
@@ -113,10 +115,10 @@ function AfterClicked() {
     document.getElementById("congrats").style.textAlign = "center";
 
     return; //Don't complete the other things I mean We did it, man
-  } else if (inputVal < megaAchieved) {   //Continue your achievements
+  } else if (inputVal < AGAchieved) {   //Continue your achievements
     document.getElementById("message").innerHTML = "(" + inputVal + ")" + "Continue.";
 
-  } if (total + 1 == megaAchieved) {   //Changing the stuff cuz we like (megaAchieved = 1;)
+  } if (total + 1 == AGAchieved) {   //Changing the stuff cuz we like (AGAchieved = 1;)
       one();  //Set the Stuff
       /*  Note for the user that there is one left  */
       var x = document.createElement("SPAN");
@@ -124,7 +126,6 @@ function AfterClicked() {
       x.innerHTML = "One Left";
     }
 
-  /*  After clicking on the button just remove the input from the field thus the button  */
-  numField.value = "";
-  $('#pressToSubmitAchievement').hide()
+
+numField.value = "";
 }
